07a4b39805f3e93137a379e241a4f7ecfee235f0
gdm <gdm.mbp@gdmMBP.local>
2019-01-06 14:52:41 -0500

    Initial commit for branch

--- Shortcut/AppDelegate.swift
+++ Shortcut/AppDelegate.swift
@@ -12,35 +12,56 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+//    var vcsArray = [UIViewController]()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        //Instantiate all VCs for shortcut
+        //NOT NEEDED FOR TABBARCONTROLLER BRANCH
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let mountainVC = storyboard.instantiateViewController(withIdentifier: "mountainVC") as! MountainsVC
+//        let spaceVC = storyboard.instantiateViewController(withIdentifier: "spaceVC") as! SpaceVC
+//        let oceanVC = storyboard.instantiateViewController(withIdentifier: "oceanVC") as! OceanVC
+//        vcsArray = [mountainVC, spaceVC, oceanVC]
+        
+        //Create dynamic shorcut item
+        let oceanShortcut = UIMutableApplicationShortcutItem(type: "\(String(describing: Bundle.main.bundleIdentifier)).ocean", localizedTitle: "Ocean", localizedSubtitle: nil, icon: UIApplicationShortcutIcon.init(templateImageName: "OceanShort"), userInfo: nil)
+        
+        application.shortcutItems = [oceanShortcut]
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    enum ShortcutType: String {
+        case mountains = "mountains"
+        case space = "space"
+        case ocean = "ocean"
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        
+        if let type = shortcutItem.type.components(separatedBy: ".").last {
+            
+            let navVC = window?.rootViewController as! UITabBarController
+//            navVC.setViewControllers(vcsArray, animated: false)
+            
+            switch type {
+            case ShortcutType.space.rawValue:
+//                navVC.popToViewController(vcsArray[1], animated: true)
+                navVC.selectedIndex = 1
+                completionHandler(true)
+            case ShortcutType.ocean.rawValue:
+                navVC.selectedIndex = 2
+//                navVC.popToViewController(vcsArray[2], animated: true)
+                completionHandler(true)
+            default:
+//                navVC.popToRootViewController(animated: true)
+                navVC.selectedIndex = 0
+                completionHandler(true)
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+            }
+        }
+        completionHandler(false)
     }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
---
+++ Shortcut/SpaceVC.swift
@@ -0,0 +1,30 @@
+//
+//  SpaceVC.swift
+//  Shortcut
+//
+//  Created by gdm on 1/6/19.
+//  Copyright © 2019 gdm. All rights reserved.
+//
+
+import UIKit
+
+class SpaceVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

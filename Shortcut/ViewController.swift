7f657dc2c9f5caa4350414e05506cd8b7da2279a
Charles Davis <tgcd5@me.com>
2019-01-06 13:19:57 -0500

    Initial Commit

---
+++ Shortcut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shortcut
+//
+//  Created by gdm on 1/6/19.
+//  Copyright © 2019 gdm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
